510086597d9ba7ef40cea5c171a1ed9e56b355e4
swe-nahidparvez <nahidparvez.2k4@gmail.com>
2026-05-30 21:46:52 +0900

    new file add

---
+++ Operator/bitwise.cpp
@@ -0,0 +1,41 @@
+// Bitwise Operator
+// Author: Nahid Parvez
+
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+
+    // & bitwise AND
+    // | bitwise OR
+    // ^ bitwise EXOR
+    // >> bitwise shift right (2 step Division)
+    // << bitwise shift left  (2 step multiplication)
+    // ~ bitwise NOT
+
+    int x = 5;
+    int y = 10;
+    int result;
+
+    result = x & y;
+
+    cout << "The X Is:" << result << endl;
+
+    result = x | y;
+
+    cout << "The X Is:" << result << endl;
+
+    result = x ^ y;
+
+    cout << "The X Is:" << result << endl;
+    result = x >> y;
+
+    cout << "The X Is:" << result << endl;
+    result = x << y;
+
+    cout << "\033[35mThe X Is:" << result << endl;
+
+    return 0;
+}
