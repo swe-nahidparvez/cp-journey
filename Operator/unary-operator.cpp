9eb4eb818bfe6bdd82fff45bf1c0d2b578d52935
swe-nahidparvez <nahidparvez.2k4@gmail.com>
2026-05-31 15:27:23 +0900

    new file add

---
+++ Operator/unary-operator.cpp
@@ -0,0 +1,24 @@
+// Special Operator
+// Author: Nahid Parvez
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+    // all unary operator (+, -)
+    // (+)-Unary Plus
+    // (-)- unary minus
+
+    int x = 5;
+    int result;
+
+    result = +x;
+
+    cout << "The X Is:" << result << endl;
+
+    return 0;
+}
